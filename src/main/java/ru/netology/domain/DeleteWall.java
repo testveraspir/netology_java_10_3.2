54ad0c445bb606b215a27b088398b1444163d4f7
Vera Spiridonova <ver-spiridonova@yandex.ru>
2021-04-12 19:53:23 +0300

    Update PostManager

--- src/main/java/ru/netology/domain/DeleteWall.java
+++
@@ -1,6 +0,0 @@
-package ru.netology.domain;
-
-public class DeleteWall {
-    private int ownerId;
-    private int postId;
-}
--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -3,5 +3,13 @@ package ru.netology.manager;
 import ru.netology.domain.Post;
 
 public class PostManager {
-    private Post[]post;
+    private Post[] post;
+
+    public void delete(int ownerId, int postId) {
+
+    }
+
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return null;
+    }
 }
